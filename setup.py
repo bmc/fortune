67b3b4a020eb7595580f0019a69e9f6c74b27a7f
Brian Clapper <bmc@clapper.org>
2008-07-19 12:13:06 +0000

    now loads long description from module

--- setup.py
+++ setup.py
@@ -11,12 +11,24 @@ from setuptools import setup, find_packages
 
 import sys
 import os
+import imp
+
+# Load the long description
+
+here = os.path.dirname(os.path.abspath(sys.argv[0]))
+sys.path = [here] + sys.path
+mf = os.path.join(here, 'fortune', '__init__.py')
+m = imp.load_module('fortune', open(mf), mf,
+                    ('__init__.py', 'r', imp.PY_SOURCE))
+long_description = m.__doc__
+
 
 # Now the setup stuff.
 
 setup (name             = 'fortune',
        version          = '0.5',
        description      = 'Python version of old BSD Unix fortune program',
+       long_description = long_description,
        packages         = find_packages(),
        url              = 'http://www.clapper.org/software/python/fortune',
        license          = 'BSD license',
