1d16d09721e93fd3c46b752b2b0ad72987ff5e2b
Brian Clapper <bmc@clapper.org>
2023-08-15 19:54:32 -0400

    Code now passes pyright and is reformatted via isort and black.

--- fortune/__init__.py
+++ fortune/__init__.py
@@ -68,39 +68,41 @@ It does, however, provide the most important function: The ability to display
 a random quote from a set of quotes.
 """
 
-__docformat__ = 'markdown'
+__docformat__ = "markdown"
 
 # ---------------------------------------------------------------------------
 # Imports
 # ---------------------------------------------------------------------------
 
-import random
-import os
-import sys
 import codecs
+import os
+import random
 import re
-
+import sys
 from optparse import OptionParser
 
-sys.stdout.reconfigure(encoding='utf-8')
+# TextIO doesn't expose reconfigure, but it's there. Tell pyright not
+# to complain.
+sys.stdin.reconfigure(encoding="utf-8")  # pyright: ignore
 
 # ---------------------------------------------------------------------------
 # Exports
 # ---------------------------------------------------------------------------
 
-__all__ = ['main', 'get_random_fortune']
+__all__ = ["main", "get_random_fortune"]
 
 # Info about the module
-__version__   = '1.1.1'
-__author__    = 'Brian M. Clapper'
-__email__     = 'bmc@clapper.org'
-__url__       = 'http://software.clapper.org/fortune/'
-__copyright__ = '2008-2023 Brian M. Clapper'
-__license__   = 'BSD-style license'
+__version__ = "1.1.1"
+__author__ = "Brian M. Clapper"
+__email__ = "bmc@clapper.org"
+__url__ = "http://software.clapper.org/fortune/"
+__copyright__ = "2008-2023 Brian M. Clapper"
+__license__ = "BSD-style license"
 
 # ---------------------------------------------------------------------------
 # Functions
 # ---------------------------------------------------------------------------
+
 
 def _random_int(start, end):
     try:
@@ -112,20 +114,21 @@ def _random_int(start, end):
 
     return r.randint(start, end)
 
+
 def _read_fortunes(fortune_file):
-    """ Yield fortunes as lists of lines """
-    with codecs.open(fortune_file, mode='r', encoding='utf-8') as f:
+    """Yield fortunes as lists of lines"""
+    with codecs.open(fortune_file, mode="r", encoding="utf-8") as f:
         contents = f.read()
 
-    lines = [line.rstrip() for line in contents.split('\n')]
+    lines = [line.rstrip() for line in contents.split("\n")]
 
-    delim = re.compile(r'^%$')
+    delim = re.compile(r"^%$")
 
     fortunes = []
     cur = []
 
     def save_if_nonempty(buf):
-        fortune = '\n'.join(buf)
+        fortune = "\n".join(buf)
         if fortune.strip():
             fortunes.append(fortune)
 
@@ -141,6 +144,7 @@ def _read_fortunes(fortune_file):
         save_if_nonempty(cur)
 
     return fortunes
+
 
 def get_random_fortune(fortune_file):
     """
@@ -158,16 +162,24 @@ def get_random_fortune(fortune_file):
     randomRecord = _random_int(0, len(fortunes) - 1)
     return fortunes[randomRecord]
 
+
 def main():
     """
     Main program.
     """
-    usage = 'Usage: %prog [OPTIONS] [fortune_file]'
+    usage = "Usage: %prog [OPTIONS] [fortune_file]"
     arg_parser = OptionParser(usage=usage)
-    arg_parser.add_option('-V', '--version', action='store_true',
-                          dest='show_version', help='Show version and exit.')
-    arg_parser.epilog = 'If fortune_file is omitted, fortune looks at the ' \
-                        'FORTUNE_FILE environment variable for the path.'
+    arg_parser.add_option(
+        "-V",
+        "--version",
+        action="store_true",
+        dest="show_version",
+        help="Show version and exit.",
+    )
+    arg_parser.epilog = (
+        "If fortune_file is omitted, fortune looks at the "
+        "FORTUNE_FILE environment variable for the path."
+    )
 
     options, args = arg_parser.parse_args(sys.argv)
     if len(args) == 2:
@@ -175,20 +187,21 @@ def main():
 
     else:
         try:
-            fortune_file = os.environ['FORTUNE_FILE']
+            fortune_file = os.environ["FORTUNE_FILE"]
         except KeyError:
-            print('Missing fortune file.', file=sys.stderr)
+            print("Missing fortune file.", file=sys.stderr)
             print(usage, file=sys.stderr)
             sys.exit(1)
 
     try:
         if options.show_version:
-            print('fortune, version {}'.format(__version__))
+            print("fortune, version {}".format(__version__))
         else:
             print(get_random_fortune(fortune_file))
     except ValueError as msg:
         print(msg, file=sys.stderr)
         sys.exit(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
